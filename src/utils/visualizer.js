f09bcd147bd8af79d72c402e2e1bb4cd128f7a75
stylate <nguyenalan@berkeley.edu>
2018-08-07 16:56:00 -0700

    syntaxing

--- src/utils/visualizer.js
+++ src/utils/visualizer.js
@@ -1,18 +1,23 @@
+var container, camera, scene, renderer;
+var uniforms, vertexShader, fragmentShader;
+var material, geometry, mesh;
+
+
 // init
 function init() {
     // renderer
-    var renderer = new THREE.WebGLRender();
+    renderer = new THREE.WebGLRender();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
     // camera
-    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
     camera.position.set(0, 0, 100);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
     camera.position.z = 2500;
 
     // scene
-    var scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
     // uniforms
     
