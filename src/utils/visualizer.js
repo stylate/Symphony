64c9a49cae0895131695965b3dc7fe291e78d958
stylate <nguyenalan@berkeley.edu>
2018-08-13 16:47:22 -0700

    updated base

--- src/utils/visualizer.js
+++ src/utils/visualizer.js
@@ -12,28 +12,39 @@ function init() {
     document.body.appendChild(renderer.domElement);
 
     scene = new THREE.Scene();
+    scene.fog = new THREE.Fog( 0x000000, 1, 1000 );
 
     // camera
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = 100;
+    camera.position.z = 2500;
+
+    // uniforms, shader components
+    uniforms = {
+        scale: { type: "f", value: 30.0 },
+        displacement: { type: "f", value: 300.0}
+    };
+
 
     // material
-    material = new THREE.MeshBasicMaterial({
+    material = new THREE.ShaderMaterial({
+        uniforms: uniforms,
         color: 0xb7ff00,
         wireframe: true
     });
 
     // mesh
-    mesh = new THREE.Mesh(new THREE.IcosahedronGeometry(20, 4), material);
+    geometry = new THREE.SphereBufferGeometry( 400, 50, 50 );
+    mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
     render();
-
 }
 
 function render() {
-    renderer.render(scene, camera);
     requestAnimationFrame(render);
+    mesh.rotation.x += 0.005;
+    mesh.rotation.y += 0.01;
+	renderer.render(scene, camera);
 }
 
 init();
