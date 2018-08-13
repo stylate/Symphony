0ebfad0020ea0433b3919b2b810482829365c10e
stylate <nguyenalan@berkeley.edu>
2018-08-13 14:03:37 -0700

    base template done

--- src/utils/visualizer.js
+++ src/utils/visualizer.js
@@ -6,29 +6,34 @@ var material, geometry, mesh;
 // init
 function init() {
     // renderer
-    renderer = new THREE.WebGLRender();
+    renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
     document.body.appendChild(renderer.domElement);
 
-    // camera
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-    camera.position.set(0, 0, 100);
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
-    camera.position.z = 2500;
-
-    // scene
     scene = new THREE.Scene();
 
-    // uniforms
-    
-    // shaders
-    
-    // geometry, mesh
+    // camera
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 100;
+
+    // material
+    material = new THREE.MeshBasicMaterial({
+        color: 0xb7ff00,
+        wireframe: true
+    });
+
+    // mesh
+    mesh = new THREE.Mesh(new THREE.IcosahedronGeometry(20, 4), material);
+    scene.add(mesh);
+
+    render();
+
 }
 
 function render() {
-
-
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
 }
 
 init();
