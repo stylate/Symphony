99bd89b5287e19855ab63bad853a90962c107f52
stylate <nguyenalan@berkeley.edu>
2018-08-14 11:00:59 -0700

    added lights, fog, postprocessing

--- src/utils/visualizer.js
+++ src/utils/visualizer.js
@@ -13,6 +13,11 @@ function init() {
 
     scene = new THREE.Scene();
     scene.fog = new THREE.Fog( 0x000000, 1, 1000 );
+    scene.add( new THREE.AmbientLight( 0x222222 ) );
+
+    light = new THREE.DirectionalLight( 0xffffff );
+	light.position.set( 1, 1, 1 );
+	scene.add( light );
 
     // camera
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
@@ -24,11 +29,11 @@ function init() {
         displacement: { type: "f", value: 300.0}
     };
 
-
     // material
     material = new THREE.ShaderMaterial({
         uniforms: uniforms,
         color: 0xb7ff00,
+        flatShading: true,
         wireframe: true
     });
 
@@ -36,6 +41,19 @@ function init() {
     geometry = new THREE.SphereBufferGeometry( 400, 50, 50 );
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
+
+	// postprocessing
+	/*composer = new THREE.EffectComposer( renderer );
+	composer.addPass( new THREE.RenderPass( scene, camera ) );
+
+	var effect = new THREE.ShaderPass( THREE.DotScreenShader );
+	effect.uniforms[ 'scale' ].value = 4;
+	composer.addPass( effect );
+
+	var effect = new THREE.ShaderPass( THREE.RGBShiftShader );
+	effect.uniforms[ 'amount' ].value = 0.0015;
+	effect.renderToScreen = true;
+	composer.addPass( effect );*/
 
     render();
 }
